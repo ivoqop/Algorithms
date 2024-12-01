0eaca13f26feb79875a92f6ee90e044daee856d1
Ivo Pereira <ivopereira@MacBook-Pro-de-Ivo.local>
2024-12-01 17:10:03 -0300

    feat: setup project and implement Binary Search algorithm

---
+++ Algorithms/ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Algorithms;
+
+Console.WriteLine("Insert int numbers: ");
+int[] array = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+Console.WriteLine("Insert target number: ");
+int target = Console.ReadLine().Split(",").Select(int.Parse).First();
+
+BinarySearch binarySearch = new BinarySearch();
+int result = binarySearch.Search(array, target);
+
+Console.WriteLine("target number on index: " + result);
